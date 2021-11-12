479c65604fb9276a7211b10139ce7b0d683d2f77
Elmira Aliyeva <elmira.ali.98@gmail.com>
2021-11-12 16:26:37 -0500

    :construction: Added models

---
+++ app/models/event.py
@@ -0,0 +1,35 @@
+import uuid
+import base64
+import re
+from ast import literal_eval
+
+class Event:
+    def __init__(self, user_id, event_name, event_description, event_location, event_start_time, event_end_time, attendee_limit, event_id=None):
+        if event_id is None:
+            self.generate_id()
+        else:
+            self.event_id = event_id
+        self.user_id = user_id
+        self.event_name = event_name
+        self.event_description = event_description
+        self.event_location = event_location
+        self.event_start_time = event_start_time
+        self.event_end_time = event_end_time
+        self.attendee_limit = attendee_limit
+    
+    def generate_id(self):
+        self.event_id = re.sub("[^0-9a-zA-Z]+", "", str(base64.b64encode(uuid.uuid4().bytes)))
+
+    def to_dict(self):
+        return {
+            'event_id': self.event_id,
+            'organizer_id': self.user_id, 
+            'title': self.event_name,
+            'description': self.event_description,
+            'location': literal_eval(self.event_location)[0], 
+            'lat': literal_eval(self.event_location)[1],
+            'long': literal_eval(self.event_location)[2],
+            'start_time': self.event_start_time,
+            'end_time': self.event_end_time,
+            'attendee_limit': self.attendee_limit,
+        }
