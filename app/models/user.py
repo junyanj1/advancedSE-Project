fb9a5f7c85afbdaf692d9fdc965d38457ac95341
Xihao Luo <47164898+XihaoLuo@users.noreply.github.com>
2021-11-13 23:06:49 -0500

    :recycle: changed class fields; deleted generate_id method

--- app/models/user.py
+++ app/models/user.py
@@ -1,26 +1,12 @@
-import uuid
-import base64
-import re
-
-
 class User:
-    def __init__(self, org_id, username, email, user_id=None):
-        if user_id is None:
-            self.generate_id()
-        else:
-            self.user_id = user_id
-        self.org_id = org_id
+    def __init__(self, user_id, org_name, username):
+        self.user_id = user_id
+        self.org_name = org_name
         self.username = username
-        self.email = email
-
-    def generate_id(self):
-        self.user_id = re.sub("[^0-9a-zA-Z]+", "",
-                              str(base64.b64encode(uuid.uuid4().bytes)))
 
     def to_dict(self):
         return {
-            'organization_id': self.org_id,
-            'username': self.username,
-            'email': self.email,
             'user_id': self.user_id,
+            'org_name': self.org_name,
+            'username': self.username
         }
