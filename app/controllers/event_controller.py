e4f11fa5bd59fe93e6c861778f6cb68aba890576
Elmira Aliyeva <elmira.ali.98@gmail.com>
2021-11-14 01:43:28 -0500

    :recycle: Refactored event controller slightly, changed event time regex to timestamp, added whitespaces to event check regex

--- app/controllers/event_controller.py
+++ app/controllers/event_controller.py
@@ -5,23 +5,21 @@ import re
 from flask import abort
 from psycopg.errors import ForeignKeyViolation, UniqueViolation
 
-ISO_8601 = r'^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9]) \
-           -(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]) \
-           :([0-5][0-9])(\.[0-9]+)?(Z|[+-](?:2[0-3]|[01][0-9]):[0-5][0-9])?$'
-
 
 class EventController():
     def __init__(self, db: Database):
         self.db = db
 
-    def get_event(self, event_id):
+    def get_event(self, event_id) -> dict:
         """
         @param: event_id: str, required
         @return: event matching given event_id
 
-        Get event details
+        Get event details from event_id
         """
-        if self.check_event_id(event_id):
+        if not event_id:
+            return abort(400, "Missing event_id")
+        if self.validate_event_id(event_id):
             row = self.db.get_one("SELECT * FROM Events WHERE event_id = %s",
                                   (event_id, ))
             if row is not None:
@@ -30,13 +28,16 @@ class EventController():
                               row['event_start_time'], row['event_end_time'],
                               row['attendee_limit'], row['event_id'])
                 return event.to_dict()
-        abort(400, 'Invalid parameter value')
+            else:
+                abort(400, "The input event_id doesn't exist")
+        else:
+            abort(400, "The input event_id is invalid")
 
     def create_event(self, organizer_id, title, description, location_name,
                      address, lat, long, start_time, end_time, attendee_limit):
         """
-        @param: organizer_id: str,
-        @param: title: str,
+        @param: organizer_id: str, required
+        @param: title: str, required
         @param: description: str,
         @param: location_name: str,
         @param: address: str,
@@ -49,9 +50,9 @@ class EventController():
 
         Create new event
         """
-        if self.check_event_input(organizer_id, title, description,
-                                  location_name, address, lat, long,
-                                  start_time, end_time, attendee_limit):
+        if self.validate_event_input(organizer_id, title, description,
+                                     location_name, address, lat, long,
+                                     start_time, end_time, attendee_limit):
             location_str = f'(\'{location_name}\', {lat}, \
                            {long}, \'{address}\')'
             event = Event(organizer_id, title, description, location_str,
@@ -71,7 +72,7 @@ class EventController():
         else:
             abort(400, 'Invalid parameter value')
 
-    def check_event_id(self, event_id):
+    def validate_event_id(self, event_id):
         """
         @param: event_id: str, required
         @return: bool, True if event_id matches pattern
@@ -80,17 +81,18 @@ class EventController():
         """
         validated = False
         try:
-            validated = Schema(And(str, lambda s: bool(
-                                   re.match(
-                                        "^[A-Za-z0-9]*$",
-                                        s)))).validate(event_id)
+            schema = Schema(And(str, lambda s: bool(
+                                   re.match("^[A-Za-z0-9]*$",
+                                            s))))
+            if schema.validate(event_id):
+                validated = True
         except SchemaError as e:
             print(e)
         return validated
 
-    def check_event_input(self, organizer_id, title, description,
-                          location_name, address, lat, long, start_time,
-                          end_time, attendee_limit):
+    def validate_event_input(self, organizer_id, title, description,
+                             location_name, address, lat, long, start_time,
+                             end_time, attendee_limit):
         """
         @param: organizer_id: str,
         @param: title: str,
@@ -104,25 +106,31 @@ class EventController():
         @param: attendee_limit: int,
         @return: bool, True if all event data matches pattern
 
-        Validate event data through regex check
+        Validate event data through regex validate
         """
         schema = Schema({
                           'organizer_id': And(str, lambda s: bool(
                               re.match("^[A-Za-z0-9]*$", s))),
                           'title': And(str, lambda s: bool(
-                              re.match("^[A-Za-z0-9]*$", s))),
+                              re.match("^[A-Za-z0-9\s]*$", s))),
                           'description': And(str, lambda s: bool(
-                              re.match("^[A-Za-z0-9.,]*$", s))),
+                              re.match("^[A-Za-z0-9.,\s]*$", s))),
                           'location_name': And(str, lambda s: bool(
-                              re.match("^[A-Za-z0-9.,]*$", s))),
+                              re.match("^[A-Za-z0-9.,\s]*$", s))),
                           'address': And(str, lambda s: bool(
-                              re.match("^[A-Za-z0-9.,]*$", s))),
+                              re.match("^[A-Za-z0-9.,\s]*$", s))),
                           'lat': float,
                           'long': float,
                           'start_time': And(str, lambda s: bool(
-                              re.match(ISO_8601, s))),
+                              re.match(
+                                       "^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]"
+                                       "|[1-2][0-9]|3[0-1]) (0[0-9]|1[0-9]"
+                                       "|2[0-3]):([0-5][0-9])$", s))),
                           'end_time': And(str, lambda s: bool(
-                              re.match(ISO_8601, s))),
+                              re.match(
+                                       "^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]"
+                                       "|[1-2][0-9]|3[0-1]) (0[0-9]|1[0-9]"
+                                       "|2[0-3]):([0-5][0-9])$", s))),
                           'attendee_limit': And(int,
                                                 lambda n:
                                                 n > 0 and n < 100000),
@@ -143,7 +151,8 @@ class EventController():
 
         validated = False
         try:
-            validated = schema.validate(data)
+            if schema.validate(data):
+                validated = True
         except SchemaError as e:
             print(e)
         return validated
