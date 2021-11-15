343eac7b9d304253737abd000bb7a0a54f521fdd
Xihao Luo <47164898+XihaoLuo@users.noreply.github.com>
2021-11-15 03:46:37 -0500

    :rewind: reverted changes to previous commit; renamed some fields

--- app/models/event.py
+++ app/models/event.py
@@ -1,6 +1,7 @@
 import uuid
 import base64
 import re
+from ast import literal_eval
 
 
 class Event:
@@ -14,9 +15,7 @@ class Event:
         self.user_id = user_id
         self.event_name = event_name
         self.event_description = event_description
-        event_location = re.sub(r"[()]", "", event_location)
-        self.event_location = tuple(map(str, event_location.split(',')))
-        print(self.event_location)
+        self.event_location = event_location
         self.event_start_time = event_start_time
         self.event_end_time = event_end_time
         self.attendee_limit = attendee_limit
@@ -29,12 +28,12 @@ class Event:
         return {
             'event_id': self.event_id,
             'user_id': self.user_id,
-            'title': self.event_name,
-            'description': self.event_description,
-            'location': self.event_location[0],
-            'lat': self.event_location[1],
-            'long': self.event_location[2],
-            'address': self.event_location[3],
+            'event_name': self.event_name,
+            'event_description': self.event_description,
+            'location': literal_eval(self.event_location)[0],
+            'lat': literal_eval(self.event_location)[1],
+            'long': literal_eval(self.event_location)[2],
+            'address': literal_eval(self.event_location)[3],
             'start_time': self.event_start_time,
             'end_time': self.event_end_time,
             'attendee_limit': self.attendee_limit,
