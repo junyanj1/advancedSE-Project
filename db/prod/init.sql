e2585d2bf622a6c689d449fc43165057cb26b8e4
Xihao Luo <47164898+XihaoLuo@users.noreply.github.com>
2021-11-15 16:28:20 -0500

    :card_file_box: added DB initializations to prod from dev

--- db/prod/init.sql
+++ db/prod/init.sql
@@ -7,18 +7,62 @@ BEGIN
 END;
 $$ LANGUAGE 'plpgsql';
 
+-- Custom Location Composite Type
+CREATE TYPE loc AS (
+    name      VARCHAR(255),
+    lat       DOUBLE PRECISION,
+    long      DOUBLE PRECISION,
+    address   VARCHAR(255)
+);
+
 -- User table
-CREATE TABLE SampleUsers (
+CREATE TABLE Users (
     user_id VARCHAR(255) UNIQUE,
+    org_name VARCHAR(255),
     username VARCHAR(255) NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE  NOT NULL  DEFAULT current_timestamp,
     updated_at TIMESTAMP WITH TIME ZONE  NOT NULL  DEFAULT current_timestamp,
     PRIMARY KEY (user_id)
 );
 
+-- Event Table
+CREATE TABLE Events (
+    event_id VARCHAR(255) UNIQUE,
+    event_name VARCHAR(255) NOT NULL,
+    user_id VARCHAR(255) NOT NULL,
+    event_description TEXT,
+    event_location loc,
+    event_start_time TIMESTAMP,
+    event_end_time TIMESTAMP,
+    attendee_limit INTEGER,
+    has_started BOOLEAN DEFAULT FALSE,  -- maybe remove later
+    has_ended BOOLEAN DEFAULT FALSE,  -- maybe remove later
+    created_at TIMESTAMP WITH TIME ZONE  NOT NULL  DEFAULT current_timestamp,
+    updated_at TIMESTAMP WITH TIME ZONE  NOT NULL  DEFAULT current_timestamp,
+    PRIMARY KEY (event_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    CONSTRAINT attendee_limit_check CHECK(attendee_limit >= 0),
+    CONSTRAINT time_validity CHECK(event_end_time >= event_start_time)
+);
+
+-- Attendence Table
+CREATE TABLE Attendance (
+    event_id VARCHAR(255) NOT NULL,
+    user_email VARCHAR(255) NOT NULL,
+    user_role VARCHAR(255),
+    personal_code VARCHAR(255),
+    is_invited BOOLEAN DEFAULT TRUE,
+    is_rsvped BOOLEAN DEFAULT FALSE,
+    is_checked_in BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP WITH TIME ZONE  NOT NULL  DEFAULT current_timestamp,
+    updated_at TIMESTAMP WITH TIME ZONE  NOT NULL  DEFAULT current_timestamp,
+    PRIMARY KEY (event_id, user_email),
+    FOREIGN KEY (event_id) REFERENCES Events
+);
+
 -- Updated_at trigger
-CREATE TRIGGER tg_sampleusers_updated_at
+CREATE TRIGGER users_updated_at
     BEFORE UPDATE
-    ON SampleUsers
+    ON Users
     FOR EACH ROW
     EXECUTE PROCEDURE update_updated_at_column();
